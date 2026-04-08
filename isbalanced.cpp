f65e242eaf94b5b5fc900294697b14e44020e775
vidya-manti23 <vidya.manti23@gmail.com>
2026-04-08 14:34:26 +0530

    Create isbalanced.cpp

---
+++ isbalanced.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isbalanced(string s){
+    stack<char>st;
+    for(char c:s){
+        
+        if(c=='{'||c=='('||c=='['){ //if open bracket
+            st.push(c);
+        }
+        
+        else if(st.empty()) return false; //if stack completed like all {({[..all pushed, now not balanced
+            
+        else if((c=='}' && st.top()=='{') || //closed bracket and matched
+                (c==']' && st.top()=='[') ||
+                (c==')' && st.top()=='(')){
+                    st.pop();
+        }
+        else{
+            return false;  //closed bracket and not matched...[{( ]}]
+        }
+            
+        }
+        
+    return st.empty();
+}
+
+int main(){
+string s;
+cin>>s;
+ if(isbalanced(s)){
+     cout<<"balanced";
+ }else{
+     cout<<"not balanced";
+ }
+    return 0;
+}
